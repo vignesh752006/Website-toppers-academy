7c80c8374b2b70ec5f05f70771ab890415333d71
vignesh752006 <vigneshshetty752006@gmail.com>
2025-06-16 12:32:11 +0530

    heroku, videos

--- toppersacademy/settings.py
+++ toppersacademy/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 ROOT_URLCONF = 'toppersacademy.urls'
 
 TEMPLATES = [
